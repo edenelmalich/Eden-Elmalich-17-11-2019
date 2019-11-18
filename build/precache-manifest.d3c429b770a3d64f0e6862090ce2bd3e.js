a3ec241f26288b01728f00ff72abf9a9b5a09f48
eden <eelmalich2@gmail.com>
2019-11-18 18:25:00 +0200

    initial commit

---
+++ build/precache-manifest.d3c429b770a3d64f0e6862090ce2bd3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66efb8cdadaff1ac2035b98ab5020d58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db4120d51bf08ff9cde8",
+    "url": "/static/css/main.b70983c9.chunk.css"
+  },
+  {
+    "revision": "75de4c85880b7685a550",
+    "url": "/static/js/2.1b693975.chunk.js"
+  },
+  {
+    "revision": "db4120d51bf08ff9cde8",
+    "url": "/static/js/main.5a911def.chunk.js"
+  },
+  {
+    "revision": "9d0830f4de43ac14ed25",
+    "url": "/static/js/runtime-main.4c4ff55e.js"
+  },
+  {
+    "revision": "77ca0ddf4077c1a991f11674abed76aa",
+    "url": "/static/media/Weather.77ca0ddf.png"
+  }
+]);

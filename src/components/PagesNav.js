7bdd17932bfc24a328599fd9994b9779ea2f91ad
eden <eelmalich2@gmail.com>
2019-11-25 14:39:55 +0200

    initial commit

--- src/components/PagesNav.js
+++ src/components/PagesNav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faHeart } from '@fortawesome/free-solid-svg-icons';
